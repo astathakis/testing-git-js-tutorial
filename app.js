c0068df118742822b6833234ac60744ab800866b
astathakis <tasos_stathakis@yahoo.com>
2022-04-05 16:47:31 +0300

    eloquent exe2.1

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-// Template literals - ES6+
-// Interpolation ${} - insert expression(value)
+//find
+//returns single instance - in this case object
+//returns first match,if no match undefined
+//great for getting unique values
 
-const name = 'john';
-const age = 25;
-const sentence = "Hey it 's " + name + ' and he is ' + age;
+const people = [
+  { name: 'bob', age: 20, position: 'developer', id: 1 },
+  { name: 'peter', age: 25, position: 'desighmer', id: 2 },
+  { name: 'susy', age: 30, position: 'the boss', id: 3 },
+  { name: 'sara', age: 40, position: 'cleaner', id: 4 },
+];
+//function by default return undefined
+const names = ['bob', 'peter', 'anna'];
+const person1 = names.find(function (name) {
+  return name === 'zelda';
+});
 
-const value = `hey it 's ${name} and he is ${age}. and here is some math ${
-  4 + 4
-}`;
+console.log(person1);
 
-console.log(value);
-console.log(sentence);
+const person = people.find(function (per) {
+  return per.id === 5;
+});
+
+console.log(person);
+
+const person2 = people.filter(function (person) {
+  return person.id === 3;
+});
+console.log(person2[0].name);
---
+++ eloquent/looping-triangle.js
@@ -0,0 +1,8 @@
+//chapter2 exe 2.1
+
+let hash = '#';
+
+while (hash.length < 8) {
+  console.log(hash);
+  hash += '#';
+}
