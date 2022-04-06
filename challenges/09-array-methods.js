c0b6194e9a70ea022993be3801a9bf15e87165dc
astathakis <tasos_stathakis@yahoo.com>
2022-04-07 00:41:24 +0300

    reduce complexity

--- challenges/09-array-methods.js
+++ challenges/09-array-methods.js
@@ -21,4 +21,33 @@ const highScores = students.filter(function (student) {
 const specificId = students.find(function (student) {
   return student.id === 1;
 });
-console.log(specificId);
+// console.log(specificId);
+
+const averageScore =
+  students.reduce(function (scoresTotal, student) {
+    //console.log(student);
+    scoresTotal += student.score;
+    return scoresTotal;
+  }, 0) / students.length;
+//console.log(averageScore);
+
+//square bracket notation dynamically adds a property to an object
+
+const subject = 'history';
+const total = {};
+
+total[subject] = 'some value';
+// console.log(total);
+
+const survey = students.reduce(function (survey, student) {
+  //console.log(student.favoriteSubject);
+  const favSubject = student.favoriteSubject;
+  if (survey[favSubject]) {
+    survey[favSubject]++;
+  } else {
+    survey[favSubject] = 1; //if subject same it overwrites
+  }
+  return survey;
+}, {});
+
+console.log(survey);
